2303d18a0ae06d76b634e493bffa4c6303fbd3bd
SaraDavis1991 <sarad@nevada.unr.edu>
2020-06-19 14:05:08 -0700

    redo bow

--- Orchestrator.py
+++ Orchestrator.py
@@ -558,7 +558,7 @@ orchestrator = Orchestrator()
 articles = orchestrator.read_data(ApplicationConstants.all_articles_random_v3_cleaned, random= False, number_of_articles = 50)
 #input("Press Enter to continue...") adds 1 G to mem
 #articles = orchestrator.get_all_articles()
-orchestrator.run_bow(articles, "store/np_cum_vec_POS_l_50.npy", "store/np_count_vec_POS_l_50.npy", "perceptron_POS_l_50.sav", False, True)
+orchestrator.run_bow(articles, "store/np_cum_vec_l_50.npy", "store/np_count_vec_l_50.npy", "perceptron_POS_l_50.sav", True, True)
 #orchestrator.run_bow(articles, "store/np_cum_vec_POS_50.npy", "store/np_count_vec_POS_50.npy", "perceptron_POS_50.sav", False, False)
 #orchestrator.pretrain_and_fineTune(dirty = True)
 #orchestrator.print_all_the_news()
