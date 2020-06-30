91f43ea8b5c6985fa1a77457b3bc8455b57a5c1e
SaraDavis1991 <sarad@nevada.unr.edu>
2020-06-29 18:40:35 -0700

    test 30

--- Visualizer.py
+++ Visualizer.py
@@ -47,7 +47,7 @@ class Visualizer():
 
 		self.genders = list(map(lambda label: 'Male' if label == 1 else 'Female', true_labels))
 		#self.markers = list(map(lambda label: '+' if label == 1 else 'o', true_labels))
-		tsne = TSNE(verbose=1, perplexity=20)
+		tsne = TSNE(verbose=1, perplexity=30)
 		results = tsne.fit_transform(weights)
 
 		self.fig, self.ax = plt.subplots()
