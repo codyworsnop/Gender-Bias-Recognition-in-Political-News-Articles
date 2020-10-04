87a46ab994cb5d5a5c2d41eae23cb27e78be594e
SaraDavis1991 <sarad@nevada.unr.edu>
2020-10-04 16:32:28 -0700

    debug

--- Orchestrator.py
+++ Orchestrator.py
@@ -666,7 +666,8 @@ class Orchestrator():
                   labels[:trainLen])  # was list_labels
         weights = net.Get_Weights()
         predictions = net.Predict(count_vectors[trainLen:])
-        print(trainLen, len(predictions), len(labels[trainLen:]),len(labels))
+        print("trainLen", str(trainLen))
+        print( len(predictions), len(labels[trainLen:]),len(labels))
 
         acc = accuracy_score(labels[trainLen:], predictions)  # was list_labels
         target_names = ['Female', 'Male']
