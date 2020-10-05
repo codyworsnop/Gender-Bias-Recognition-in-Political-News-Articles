913241f7ae3380a60e96567b0f9f6784c9d41ff1
SaraDavis1991 <sarad@nevada.unr.edu>
2020-10-04 19:01:58 -0700

    moving test to run_BOW.py

--- run_BOW.py
+++ run_BOW.py
@@ -4,6 +4,13 @@
 #################################
 
 from Orchestrator import Orchestrator
+import ApplicationConstants
+import spacy
+import numpy as np
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+import pickle
+from Models.SVM_engine import SVM
 
 orchestrator = Orchestrator()
 
@@ -21,7 +28,7 @@ NOTE: to run this file, articles must have been collected and run_preprocessor.p
 '''
 
 #OPTION 1: run run_bow on all words in vocab, without lemma, print vocab to confirm data is cleaned properly
-orchestrator.run_bow( "store/np_cumulative_vec_ALLnoL_HC.npy", "store/np_count_vec_ALLnoL_HC.npy", "store/perceptron_ALLnoL_HC.sav", "Hillary_Clinton", True, False, True, True, False) #notPos, lemmad, printvocab
+#orchestrator.run_bow( "store/np_cumulative_vec_ALLnoL_HC.npy", "store/np_count_vec_ALLnoL_HC.npy", "store/perceptron_ALLnoL_HC.sav", "Hillary_Clinton", True, False, True, True, False) #notPos, lemmad, printvocab
 
 #OPTION 2: run run_bow on adjectives in vocab, without lemma, print vocab to confirm proper cleaning
 #orchestrator.run_bow( "store/np_cumulative_vec_ADJnoL_4_all.npy", "store/np_count_vec_ADJnoL_4_all.npy", "store/perceptron_ADJnoL_4_all.sav", HillaryClinton, False, False, True, False) #Pos, lemmad, printvocab
@@ -35,3 +42,80 @@ orchestrator.run_bow( "store/np_cumulative_vec_ALLnoL_HC.npy", "store/np_count_v
 
 #OPTION 5: run run_bow on all words in vocab without lemma, print vocab to confirm that data is cleaned properly, run it on data in folds
 #orchestrator.run_bow("store/np_cumulative_vec_ALLnoLFolds.")
+
+
+
+def one_person_holdOut():
+    person = "Hillary_Clinton"
+    articles = orchestrator.read_data(path=ApplicationConstants.all_articles_random_v4_cleaned_pos_candidate_names,
+        number_of_articles=50, save=False)
+    cumulative_word_vec = orchestrator.calc_word_vector(articles, True, False, True)
+
+
+    articles_list = list(map(lambda leaning: articles[4][leaning][ApplicationConstants.Train] +
+                                             articles[4][leaning][ApplicationConstants.Validation] +
+                                             articles[4][leaning][ApplicationConstants.Test], articles[4]))
+    articles_list = [item for sublist in articles_list for item in sublist]
+    train_articles = list(filter(lambda article: article.Label.TargetName != person,
+                                 articles_list))  # change this line
+    train_content = list(map(lambda article: article.Content, train_articles))
+    train_label = list(map(lambda article: article.Label.TargetGender, train_articles))
+
+
+    test_articles = list(filter(lambda article: article.Label.TargetName == person,
+                                articles_list))  # change this line
+    test_content = list(map(lambda article: article.Content, test_articles))
+    test_label = list(map(lambda article: article.Label.TargetGender, test_articles))
+
+    labels = train_label + test_label
+    for i, label in enumerate(labels):  # was list_labels
+        if label == 0:
+            labels[i] = -1  # was list_labels
+    count_vectors = []
+    i = 0
+    nlp = spacy.load("en_core_web_lg")
+    for article in train_content:
+        count_vectors.append(orchestrator.calc_count_doc_count_vector(cumulative_word_vec, article, nlp, False))
+    for article in test_content:
+        count_vectors.append(orchestrator.calc_count_doc_count_vector(cumulative_word_vec, article, nlp, False))
+
+    trainLen = len(train_articles)
+
+    acc = 0
+    print("building net")
+    net = SVM()
+    print("training")
+    # print(len(labels), len(labels[:trainLen]))
+    print(len(count_vectors), len(count_vectors[:trainLen]), len(count_vectors[trainLen:]))
+    net.Train(count_vectors[:trainLen], labels[:trainLen], count_vectors[:trainLen],
+              labels[:trainLen])  # was list_labels
+    weights = net.Get_Weights()
+    predictions = net.Predict(count_vectors[trainLen:])
+    # print("trainLen", str(trainLen))
+    print(len(predictions), labels[-10:], len(labels))
+
+    acc = accuracy_score(labels[trainLen:], predictions)  # was list_labels
+    target_names = ['Female', 'Male']
+    print("accuracy is: " + str(acc))
+
+    # if the accuracy is high enough, print the metrics, and print top words to a file
+    if acc >= 0.60:
+        print(classification_report(labels[trainLen:], predictions, target_names=target_names))  # was list_labels
+
+        weights = weights[0]
+
+        resTop = sorted(range(len(weights)), key=lambda sub: weights[sub])[-25:]
+        resBottom = sorted(range(len(weights)), key=lambda sub: weights[sub])[:25]
+        model_name_amp ="store/perceptron_ALLnoL_HC.sav"
+        pickle.dump(net, open(model_name_amp, 'wb'))
+        fout = open('output_words' + person + '.txt', 'w')
+
+        fout.write("Male Top Words: \n")
+        for index in resTop:
+            fout.write(cumulative_word_vec[index] + ' ' + str(float(weights[index])) + '\n')
+        fout.write("Female Top Words: \n")
+        for index in resBottom:
+            fout.write(cumulative_word_vec[index] + ' ' + str(float(weights[index])) + '\n')
+
+
+one_person_holdOut()
